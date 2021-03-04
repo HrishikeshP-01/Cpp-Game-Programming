460a5b588e61287ef03398437cd639353456722d
HrishikeshP-01 <62914161+HrishikeshP-01@users.noreply.github.com>
2021-03-05 00:32:38 +0530

    Made a basic pong game

---
+++ 4_SimplePong/Main.cpp
@@ -0,0 +1,14 @@
+// A single player pong game
+
+#include "Game.h"
+#include <iostream>
+
+int main(int argc, char** argv) {
+	Game game;
+	bool success = game.Initialize();
+	if (success) {
+		game.RunLoop();
+	}
+	game.Shutdown();
+	return 0;
+}
